be1d1307817857c250ba6559c7390c7afc22346e
anuraag <anuraag.warudkar@gmail.com>
2023-07-25 22:44:40 -0400

    add grocery list and search

--- src/pages/GroceryList.jsx
+++ src/pages/GroceryList.jsx
@@ -1,5 +1,25 @@
 const GroceryList = () => {
-  return <h1>List</h1>;
+  return (
+    <div className="grocery-container">
+      <div className="list-heading">
+        <h1>Grocery List</h1>
+      </div>
+
+      <div className="list">
+        <ul className="list-none space-y-2 text-gray-800">
+          <li className="bg-gray-100 p-4 rounded-md shadow-sm hover:shadow-md transition-shadow">
+            Item 1
+          </li>
+          <li className="bg-gray-100 p-4 rounded-md shadow-sm hover:shadow-md transition-shadow">
+            Item 2
+          </li>
+          <li className="bg-gray-100 p-4 rounded-md shadow-sm hover:shadow-md transition-shadow">
+            Item 3
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default GroceryList;
